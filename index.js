921be8c07081c4c8260c53bc9fb253a56093fb08
Melissa Fields <melissamfields12@gmail.com>
2022-09-27 14:34:58 -0700

    commit tree 35deed1d5045a2d9c042ab32b1dfad721ad33284

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    return cats.splice(-1);
+}
+function destructivelyRemoveFirstCat() {
+    return cats.splice(0, 1);
+}
+function appendCat() {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+function prependCat() {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+function removeLastCat() {
+    const newCats = [...cats.slice(0, 2)];
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats = [...cats.slice(1)];
+    return newCats;
+}
